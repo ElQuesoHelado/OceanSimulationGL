ff63766f57247eaae3acc6f01d2cdc2408993814
MisaelPalomino <misaeljpalominor@gmail.com>
2026-07-12 16:58:36 -0500

    mando el mesh a la GPU con todos sus atributos, tengo sueño

--- src/mesh.rs
+++ src/mesh.rs
@@ -73,3 +73,74 @@ pub struct Mesh {
     index_count: i32,
     pub data: MeshData,
 }
+
+impl Mesh {
+    pub fn upload(gl: &glow::Context, data: MeshData) -> Self {
+        unsafe {
+            let vao = gl.create_vertex_array().unwrap();
+            let vbo_positions = gl.create_buffer().unwrap();
+            let vbo_normals = gl.create_buffer().unwrap();
+            let vbo_texcoords = gl.create_buffer().unwrap();
+            let ebo = gl.create_buffer().unwrap();
+
+            gl.bind_vertex_array(Some(vao));
+            gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo_positions));
+            gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo_normals));
+            gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo_texcoords));
+            gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(ebo));
+            gl.bind_vertex_array(None);
+            gl.bind_buffer(glow::ARRAY_BUFFER, None);
+            gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, None);
+
+            gl.named_buffer_data_u8_slice(
+                vbo_positions,
+                bytemuck::cast_slice(&data.positions),
+                glow::STATIC_DRAW,
+            );
+            gl.named_buffer_data_u8_slice(
+                vbo_normals,
+                bytemuck::cast_slice(&data.normals),
+                glow::STATIC_DRAW,
+            );
+            gl.named_buffer_data_u8_slice(
+                vbo_texcoords,
+                bytemuck::cast_slice(&data.texcoords),
+                glow::STATIC_DRAW,
+            );
+            gl.named_buffer_data_u8_slice(
+                ebo,
+                bytemuck::cast_slice(&data.indices),
+                glow::STATIC_DRAW,
+            );
+
+            // posiciones -> binding 0
+            gl.vertex_array_vertex_buffer(vao, 0, Some(vbo_positions), 0, 3 * 4);
+            gl.enable_vertex_array_attrib(vao, 0);
+            gl.vertex_array_attrib_format_f32(vao, 0, 3, glow::FLOAT, false, 0);
+            gl.vertex_array_attrib_binding_f32(vao, 0, 0);
+
+            // normales -> binding 1
+            gl.vertex_array_vertex_buffer(vao, 1, Some(vbo_normals), 0, 3 * 4);
+            gl.enable_vertex_array_attrib(vao, 1);
+            gl.vertex_array_attrib_format_f32(vao, 1, 3, glow::FLOAT, false, 0);
+            gl.vertex_array_attrib_binding_f32(vao, 1, 1);
+
+            // uv -> binding 2
+            gl.vertex_array_vertex_buffer(vao, 2, Some(vbo_texcoords), 0, 2 * 4);
+            gl.enable_vertex_array_attrib(vao, 2);
+            gl.vertex_array_attrib_format_f32(vao, 2, 2, glow::FLOAT, false, 0);
+            gl.vertex_array_attrib_binding_f32(vao, 2, 2);
+
+            gl.vertex_array_element_buffer(vao, Some(ebo));
+            Self {
+                vao,
+                vbo_positions,
+                vbo_normals,
+                vbo_texcoords,
+                ebo,
+                index_count: data.indices.len() as i32,
+                data,
+            }
+        }
+    }
+}
